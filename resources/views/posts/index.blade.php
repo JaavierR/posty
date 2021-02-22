b92fad6b5ee371f445cb73f530fda70e9b805c33
Javier Reyes <javier.reys@gmail.com>
2021-02-21 21:29:31 -0300

    feat: add styles to the overall layout

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,5 +1,5 @@
-@extends('layouts.app')
-
-@section('content')
-    Index
+@extends('layouts.app') @section('content')
+<div class="flex justify-center">
+    <div class="bg-white w-8/12 p-6 rounded-lg">Post</div>
+</div>
 @endsection
