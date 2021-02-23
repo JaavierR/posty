92bd146e369982951fdac9900a7981fc5d6b4ab4
Javier Reyes <javier.reys@gmail.com>
2021-02-23 18:17:57 -0300

    feat: add user profile page.

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -32,7 +32,7 @@
         @if ($posts->count())
         @foreach ($posts as $post)
         <div class="rounded shadow p-4 bg-gray-50">
-            <a href="" class="font-bold">{{ $post->user->name }}</a>
+            <a href="{{ route('users.posts', $post->user) }}" class="font-bold">{{ $post->user->name }}</a>
             <span class="text-gray-600 text-sm">{{ $post->created_at->diffForHumans() }}</span>
             <div>
                 {{ $post->body }}
